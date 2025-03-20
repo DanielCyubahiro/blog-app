b3022697c5cb3bd2b53ab4f492b1611a09e2b64a
Daniel <cyubahirodaniel@gmail.com>
2025-03-20 14:48:43 +0100

    Add homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,52 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import Post from '../components/Post.jsx';
+import {Empty, Spin} from 'antd';
+
 const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    void (async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+            'https://jsonplaceholder.typicode.com/posts');
+        setPosts(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
   return (
       <main>
+        {!loading
+            ? posts.length
+                ? (
+                    posts.map((post) => (
+                        <Post key={post.id} post={post}/>
+                    ))
+                )
+                : <Empty
+                    description={'No Posts to display'}
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                    }}
+                />
+            : <Spin
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+                size="large"
+            />
+        }
       </main>
   );
 };
