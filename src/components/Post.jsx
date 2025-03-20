da69ffaf2d7c9430b5a93a522c04c84d18077434
Daniel <cyubahirodaniel@gmail.com>
2025-03-20 14:49:06 +0100

    Add post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import {EllipsisOutlined} from '@ant-design/icons';
+import {Avatar, Card} from 'antd';
+import {useNavigate} from 'react-router-dom';
+
+const Post = ({post}) => {
+  const {Meta} = Card;
+  const navigate = useNavigate();
+  return (
+      <Card
+          style={{
+            width: 300,
+            margin: 10,
+          }}
+          actions={[
+            <EllipsisOutlined
+                key="ellipsis"
+                onClick={() => navigate('/post/' + post.id)}/>,
+          ]}
+      >
+        <Meta
+            avatar={<Avatar
+                src="https://api.dicebear.com/7.x/miniavs/svg?seed=8"/>}
+            title={post.title}
+            description={post.body.length < 25 ? post.body : post.body.slice(0,
+                70) + '...'}
+        />
+      </Card>
+  );
+};
+
+export default Post;
